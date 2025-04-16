b145b3b94980b8d5c445382ec80889aea10e5ce2
Jonathan Catalan <jluiscatalan1@gmail.com>
2025-04-16 19:59:57 -0400

    lab4_resuelto

---
+++ lib/Pages/home.page.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:logger/logger.dart';
+
+
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+  
+}
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  final logger = Logger();  
+
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+    logger.i("Incremented counter: $_counter");  
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+    logger.i("Decremented counter: $_counter");  
+  }
+
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
+    logger.i("Counter reset");  
+  }
+
+  
+  Widget _buildBottomButtons() {
+    return BottomAppBar(
+      shape: const CircularNotchedRectangle(),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: _decrementCounter,
+              icon: const Icon(Icons.remove, size: 30),
+              tooltip: 'Decrementar',
+            ),
+            IconButton(
+              onPressed: _resetCounter,
+              icon: const Icon(Icons.refresh, size: 30),
+              tooltip: 'Reiniciar',
+            ),
+            IconButton(
+              onPressed: _incrementCounter,
+              icon: const Icon(Icons.add, size: 30),
+              tooltip: 'Incrementar',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Agregar log aquí para cada vez que se construye el widget
+    logger.i("Rebuilding MyHomePage");
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SvgPicture.asset(
+              'Assets/Icons/ICON_GAME.svg',
+              semanticsLabel: 'Dart Logo',
+            ),
+            const Text('Has presionado el botón muchas veces:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: _buildBottomButtons(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:logger/logger.dart';
+import 'pages/home.page.dart'; 
+
+
+final logger = Logger();
 
 void main() {
   runApp(const MyApp());
@@ -9,108 +14,45 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {  
+    logger.i("Logger is working!");
+    const appName = 'Custom Themes';
+
     return MaterialApp(
-      title: 'Flutter Proyect Demo',
+      title: appName,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 239, 6, 41)),
-      ),
-      home: const MyHomePage(title: 'First Flutter Project'),
-    );
-  }
-}
+        // Define the default brightness and colors.
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.purple,
+          // TRY THIS: Change to "Brightness.light"
+          //           and see that all colors change
+          //           to better contrast a light background.
+          brightness: Brightness.dark,
+        ),
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  
-  void _resetCounter() {
-    setState(() {
-      _counter = 0;
-    });
-  }
-
-  
-  Widget _buildBottomButtons() {
-    return BottomAppBar(
-      shape: const CircularNotchedRectangle(),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            
-            IconButton(
-              onPressed: _decrementCounter,
-              icon: const Icon(Icons.remove, size: 30),
-              tooltip: 'Decrementar',
-            ),
-            
-            IconButton(
-              onPressed: _resetCounter,
-              icon: const Icon(Icons.refresh, size: 30),
-              tooltip: 'Reiniciar',
-            ),
-            
-            IconButton(
-              onPressed: _incrementCounter,
-              icon: const Icon(Icons.add, size: 30),
-              tooltip: 'Incrementar',
-            ),
-          ],
+        // Define the default `TextTheme`. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: TextTheme(
+          displayLarge: const TextStyle(
+            fontSize: 72,
+            fontWeight: FontWeight.bold,
+          ),
+          // TRY THIS: Change one of the GoogleFonts
+          //           to "lato", "poppins", or "lora".
+          //           The title uses "titleLarge"
+          //           and the middle text uses "bodyMedium".
+          titleLarge: GoogleFonts.oswald(
+            fontSize: 30,
+            fontStyle: FontStyle.italic,
+          ),
+          bodyMedium: GoogleFonts.merriweather(),
+          displaySmall: GoogleFonts.pacifico(),
         ),
       ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SvgPicture.asset(
-  'Assets/Icons/ICON_GAME.svg',
-  semanticsLabel: 'Dart Logo',
-),
-            const Text('Has presionado el botón muchas veces:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      
-      bottomNavigationBar: _buildBottomButtons(),
+      home: const MyHomePage(title: appName),
     );
   }
 }
+
+
+
